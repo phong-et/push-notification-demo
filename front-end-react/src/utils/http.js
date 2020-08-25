d6b88200a8563b46a226440652882ec8b9409f85
Phillip <phillip7.et@gmail.com>
2020-08-25 20:44:51 +0700

    change port 8888

--- front-end-react/src/utils/http.js
+++ front-end-react/src/utils/http.js
@@ -1,4 +1,4 @@
-const host = process.env.NODE_ENV === "production" ? "https://push-notification-demo-server.herokuapp.com" : "http://localhost:8080";
+const host = process.env.NODE_ENV === "production" ? "https://push-notification-demo-server.herokuapp.com" : "http://localhost:8888";
 
 function post(path, body) {
   return fetch(`${host}${path}`, {
